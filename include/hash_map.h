dda42e64d4d9587a59eefc22f43a8838cf5431ef
xayler4 <andrea.pastore04@outlook.com>
2022-11-17 18:31:35 +0100

    updated hash_map

--- include/hash_map.h
+++ include/hash_map.h
@@ -1,88 +1,167 @@
 #pragma once
 
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <cstring>
+#include <assert.h>
+
 template<typename TValue>
 class HashNode {
-    HashNode(TValue&& value, const unsigned int hash_code) : m_value(value), m_hash_code(hash_code) {}
+public:
+    HashNode(TValue&& value, unsigned int hash_code) : m_value(std::move(value)), m_hash_code(hash_code) {}
+
+    HashNode(TValue& value, unsigned int hash_code) : m_value(value), m_hash_code(hash_code) {}
 
     inline operator TValue&() { return m_value; }
+    inline unsigned int get_hash_code() { return m_hash_code; }
+    inline TValue& get_value() { return m_value; }
 
 private:
     TValue m_value;
-    const unsigned int m_hash_code;
+    unsigned int m_hash_code;
 };
 
 template<typename TKey>
 class HashCode {
-    static unsigned int hash_code (const TKey&& key) {
+public:
+    static unsigned int hash_code (const TKey& key) {
         return key;
     }
 };
 
 class DivisionHashIndex {
+public:
     static unsigned int hash_index (unsigned int hash_code, unsigned int size) {
         return hash_code % size;
     }
 };
 
-class OpenAddressing {
+template<typename TValue, typename TKey, float TGrowthFactor = 0.75f, typename THashIndex = DivisionHashIndex, typename TAllocator = std::allocator<HashNode<TValue>>>
+class HashMapOpenAddressingLinearProbing {
 public:
-    template<typename TValue, typename TKey>
-    class LinearProbing {
-    public:
-        using T = TValue;
-        using K = TKey;
+    HashMapOpenAddressingLinearProbing() : m_count(0), m_size(2){
+        m_data = m_allocator.allocate(m_size);
+        std::memset(m_data, 0, m_size * sizeof(HashNode<TValue>));
+    }
 
-    public:
-        LinearProbing() = default;
-        ~LinearProbing() = default;
+    ~HashMapOpenAddressingLinearProbing() = default;
 
-        template<typename... TArgs>
-        HashNode<TValue> emplace(unsigned int hash_index, TArgs&&... args) {
-            
-        }
-
-        HashNode<TValue>& insert(unsigned int hash_index, TValue&& value) {
-            
-        }
-
-        HashNode<TValue>& search(unsigned int hash_index) {
-
-        }
-
-        void remove(unsigned int hash_index, unsigned int hash_code) {
-
-        }
-
-        LinearProbing (const LinearProbing&) = delete;
-        LinearProbing& operator = (const LinearProbing&) = delete;
-
-    };
-
-public:
-    OpenAddressing() = delete;
-    OpenAddressing(const OpenAddressing&) = delete;
-    OpenAddressing& operator = (const OpenAddressing&) = delete;
-};
-
-template<typename TCollisionHandler, typename THashIndex = DivisionHashIndex>
-class HashMap {
-public:
-    HashMap() = default;
-    ~HashMap() = default;
-    
     template<typename... TArgs>
-    inline HashNode& emplace(typename TCollisionHandler::K&& key, TArgs&&... args) {
-        return m_collision_handler.emplace(key, std::forward<TArgs>(args)...);
+    HashNode<TValue>& emplace(TKey& key, TArgs&&... args) {
+        unsigned int hash_index = THashIndex::hash_index(HashCode<TValue>::hash_code(key), m_size);
+
+        while (m_data[hash_index].get_hash_code()) {
+            if (++hash_index == m_size) {
+                grow(m_size + (m_size * TGrowthFactor));
+                return emplace(key, std::forward<TArgs>(args)...);
+            }
+        }
+        m_data[hash_index] = HashNode<TValue>(T(std::forward<TArgs>(args)...));
+        m_count++;
+
+        return m_data[hash_index];
     }
 
-    inline HashNode& insert(typename TCollisionHandler::K&& key, typename TCollisionHandler::T&& value ) {
-        return m_collision_handler.insert(THashIndex::hash_index(HashCode<T>::hash_code(key)), value);
+    HashNode<TValue>& insert(const TKey& key, TValue&& value ) {
+        unsigned int hash_code = HashCode<TValue>::hash_code(key);
+        unsigned int hash_index = THashIndex::hash_index(hash_code, m_size);
+
+        while (m_data[hash_index].get_hash_code()) {
+            if (++hash_index == m_size) {
+                grow(m_size + (m_size * TGrowthFactor));
+                return insert(key, std::forward<TValue>(value));
+            }
+        }
+        m_data[hash_index] = HashNode<TValue>(std::forward<TValue>(value), hash_code);
+        m_count++;
+
+        return m_data[hash_index];
     }
 
-    inline HashNode& operator[] (typename TCollisionHandler::K&& key) { 
-        return m_collision_handler.search(THashIndex::hash_index(HashCode<T>::hash_code(key))); 
+    HashNode<TValue>& insert(const TKey& key, TValue& value ) {
+        unsigned int hash_code = HashCode<TValue>::hash_code(key);
+        unsigned int hash_index = THashIndex::hash_index(hash_code, m_size);
+
+        while (m_data[hash_index].get_hash_code()) {
+            if (++hash_index == m_size) {
+                grow(m_size + (m_size * TGrowthFactor));
+                return insert(key, value);
+            }
+        }
+        m_data[hash_index] = HashNode<TValue>(value, hash_code);
+        m_count++;
+
+        return m_data[hash_index];
+    }
+
+    HashNode<TValue>& operator [] (const TKey& key) {
+        unsigned int hash_code = HashCode<TValue>::hash_code(key);
+        unsigned int hash_index = THashIndex::hash_index(hash_code, m_size);
+        
+        while (m_data[hash_index].get_hash_code() != hash_code) {
+
+            hash_index++;
+            assert(hash_index != m_size);
+        }
+
+        return m_data[hash_index];
+    }
+
+    void remove(const TKey& key) {
+        unsigned int hash_code = HashCode<TValue>::hash_code(key);
+        unsigned int hash_index = THashIndex::hash_index(hash_code, m_size);
+
+        while (m_data[hash_index].get_hash_code() != hash_code) {
+            hash_index++;
+            assert(hash_index != m_size);
+        }
+
+        m_data[hash_index].~HashNode<TValue>();
+        std::memset(m_data + hash_index, 0, sizeof(HashNode<TValue>));
+    }
+
+
+    void print() {
+        for (unsigned int i = 0; i < m_size; i++) {
+            std::cout << m_data[i] << " ";
+        }
+
+        std::cout << "\n";
+    }
+
+    HashMapOpenAddressingLinearProbing (const HashMapOpenAddressingLinearProbing&) = delete;
+    HashMapOpenAddressingLinearProbing& operator = (const HashMapOpenAddressingLinearProbing&) = delete;
+
+private:
+    void grow(unsigned int new_size) {
+        unsigned int old_size = m_size;
+        m_size = new_size;
+        HashNode<TValue>* tmp_data = m_data;
+        m_data = m_allocator.allocate(m_size);
+        std::memset(m_data, 0, m_size * sizeof(HashNode<TValue>));
+
+        for (unsigned int i = 0; i < old_size; i++) {
+            if (tmp_data[i].get_hash_code()) {
+                unsigned int hash_index = THashIndex::hash_index(tmp_data[i].get_hash_code(), m_size);
+                while (m_data[hash_index].get_hash_code()) {
+                    if (++hash_index == m_size) {
+                        grow(m_size + (m_size * TGrowthFactor));
+                        m_allocator.deallocate(tmp_data, old_size * sizeof(HashNode<TValue>));
+                        return;
+                    }
+                }
+                m_data[hash_index] = std::move(tmp_data[i]);
+            }
+        }
+        m_allocator.deallocate(tmp_data, old_size * sizeof(HashNode<TValue>));
     }
 
 private:
-    TCollisionHandler m_collision_handler;
+    TAllocator m_allocator;
+    HashNode<TValue>* m_data;
+    unsigned int m_count;
+    unsigned int m_size;
+
 };
+
