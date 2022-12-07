d14aff482f90cdec97aa26341ad2ef5d82563d13
xayler4 <andrea.pastore04@outlook.com>
2022-12-07 16:39:28 +0100

    bitmask can now be used with more than 64 bits

--- include/bitmask.h
+++ include/bitmask.h
@@ -1,31 +1,38 @@
 #pragma once
 
+#include <iostream>
+#include <cmath>
 #include <cstdint>
 #include <utility>
 #include <memory>
+#include <array>
 #include <assert.h>
 
 namespace ccnt {
-    template<std::uint8_t TBits>
+    template<std::uint32_t TBits>
     class Bitmask {
     public:
+        static constexpr std::uint32_t size() {
+            float i = (float)TBits / 64;
+            return ceil(TBits);
+        } 
         using uint_t = std::conditional_t<(TBits <= 8), std::uint8_t,
                 std::conditional_t<(TBits <= 16), std::uint16_t,
                 std::conditional_t<(TBits <= 32), std::uint32_t,
                 std::conditional_t<(TBits <= 64), std::uint64_t,
-                std::nullptr_t>>>>;
+                std::array<uint64_t, size()>>>>>;
 
         class Iterator {
         public:
-            Iterator(Bitmask<TBits>& bitmask, std::uint8_t nbit) : m_bitmask(bitmask), m_nbit(nbit) {}
+            Iterator(Bitmask<TBits>* bitmask, std::uint32_t nbit) : m_bitmask(bitmask), m_nbit(nbit) {}
             ~Iterator() = default;
 
             bool operator * () {
-                return m_bitmask[m_nbit];
+                return (*m_bitmask)[m_nbit];
             }
 
             bool operator -> () {
-                return m_bitmask[m_nbit];
+                return (*m_bitmask)[m_nbit];
             }
 
             void operator ++ () {
@@ -41,102 +48,199 @@ namespace ccnt {
             }
 
         private:
-            Bitmask<TBits>& m_bitmask;
-            std::uint8_t m_nbit;
+            Bitmask<TBits>* m_bitmask;
+            std::uint32_t m_nbit;
         };
 
     public:
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
         Bitmask() : m_bitmask(0) {}
+
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        Bitmask() : m_bitmask{0} {}
+
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
         Bitmask(uint_t bitmask) : m_bitmask(bitmask) {}
 
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        Bitmask(uint_t& bitmask) {
+            for (uint32_t i = 0; i < size(); i++) {
+                m_bitmask[i] = bitmask[i]; 
+            } 
+        }
+
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
         inline Bitmask<TBits> operator & (uint_t bitmask) {
             return Bitmask<TBits>(m_bitmask & bitmask);
         }
 
-        inline void operator &= (uint_t bitmask) {
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline Bitmask<TBits>&& operator & (uint_t bitmask) {
+            for (std::uint32_t i = 0; i < size(); i++) {
+                bitmask[i] &= m_bitmask[i]; 
+            } 
+            return Bitmask<TBits>(bitmask);
+        }
+
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline void operator &= (uint_t& bitmask) {
             m_bitmask &= bitmask;
         }
 
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline void operator &= (uint_t& bitmask) {
+            for (std::uint32_t i = 0; i < size(); i++) {
+                m_bitmask[i] &= bitmask[i]; 
+            } 
+        }
+
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
         inline Bitmask<TBits> operator | (uint_t bitmask) {
             return Bitmask<TBits>(m_bitmask | bitmask);
         }
 
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline Bitmask<TBits>&& operator | (uint_t bitmask) {
+            for (std::uint32_t i = 0; i < size(); i++) {
+                bitmask[i] |= m_bitmask[i]; 
+            } 
+            return Bitmask<TBits>(bitmask);
+        }
+
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
         inline void operator |= (uint_t bitmask) {
             m_bitmask |= bitmask;
         }
 
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline void operator |= (uint_t& bitmask) {
+            for (std::uint32_t i = 0; i < size(); i++) {
+                m_bitmask[i] |= bitmask[i]; 
+            } 
+        }
+
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
         inline bool operator == (uint_t bitmask) {
             return m_bitmask == bitmask;
         }
 
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline bool operator == (uint_t& bitmask) {
+            for (std::uint32_t i = 0; i < size(); i++) {
+                if (m_bitmask[i] != bitmask[i]) {
+                    return false;
+                }
+            } 
+            return true;
+        }
+
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline bool operator != (uint_t bitmask) {
+            return m_bitmask != bitmask;
+        }
+
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline bool operator != (uint_t& bitmask) {
+            for (std::uint32_t i = 0; i < size(); i++) {
+                if (m_bitmask[i] != bitmask[i]) {
+                    return true;
+                }
+            } 
+            return false;
+        }
+
         template<typename TBitmask, typename std::enable_if<std::is_same<typename TBitmask::uint_t, uint_t>::value, std::nullptr_t>::type = nullptr>
-        inline Bitmask<TBits> operator & (TBitmask bitmask) {
-            return Bitmask<TBits>(m_bitmask & bitmask.m_bitmask);
+        inline Bitmask<TBits>&& operator & (TBitmask& bitmask) {
+            return Bitmask<TBits>(*this & bitmask.m_bitmask);
         }
         
         template<typename TBitmask, typename std::enable_if<std::is_same<typename TBitmask::uint_t, uint_t>::value, std::nullptr_t>::type = nullptr>
-        inline void operator &= (TBitmask bitmask) {
-            m_bitmask &= bitmask.m_bitmask;
+        inline void operator &= (TBitmask& bitmask) {
+            *this &= bitmask.m_bitmask;
         }
         
         template<typename TBitmask, typename std::enable_if<std::is_same<typename TBitmask::uint_t, uint_t>::value, std::nullptr_t>::type = nullptr>
-        inline Bitmask<TBits> operator | (TBitmask bitmask) {
-            return Bitmask<TBits>(m_bitmask | bitmask.m_bitmask);
+        inline Bitmask<TBits>&& operator | (TBitmask& bitmask) {
+            return Bitmask<TBits>(*this | bitmask.m_bitmask);
         }
         
         template<typename TBitmask, typename std::enable_if<std::is_same<typename TBitmask::uint_t, uint_t>::value, std::nullptr_t>::type = nullptr>
-        inline void operator |= (TBitmask bitmask) {
-            m_bitmask |= bitmask.m_bitmask;
+        inline void operator |= (TBitmask& bitmask) {
+            *this |= bitmask.m_bitmask;
         }
         
         template<typename TBitmask>
-        inline bool operator == ( TBitmask bitmask) {
-            return m_bitmask == bitmask.m_bitmask;
+        inline bool operator == ( TBitmask& bitmask) {
+            return *this == bitmask.m_bitmask;
         }
 
         template<typename TBitmask>
-        inline bool operator != ( TBitmask bitmask) {
-            return m_bitmask != bitmask.m_bitmask;
+        inline bool operator != ( TBitmask& bitmask) {
+            return *this != bitmask.m_bitmask;
         }
         
         template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::uint8_t>::value, std::nullptr_t>::type = nullptr>
-        inline bool operator [] (std::uint8_t nbit) const {
+        inline bool operator [] (std::uint32_t nbit) const {
             assert(nbit < TBits);
-            return (m_bitmask << (nbit)) & 0x80;
+            return (m_bitmask << (nbit + (sizeof(uint_t) * 8 - TBits))) & 0x80;
         }
 
         template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::uint16_t>::value, std::nullptr_t>::type = nullptr>
-        inline bool operator [] (std::uint8_t nbit) const {
+        inline bool operator [] (std::uint32_t nbit) const {
             assert(nbit < TBits);
-            return (m_bitmask << (nbit)) & 0x8000;
+            return (m_bitmask << (nbit + (sizeof(uint_t) * 8 - TBits))) & 0x8000;
         }
 
         template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::uint32_t>::value, std::nullptr_t>::type = nullptr>
-        inline bool operator [] (std::uint8_t nbit) const {
+        inline bool operator [] (std::uint32_t nbit) const {
             assert(nbit < TBits);
-            return (m_bitmask << (nbit)) & 0x80000000;
+            return (m_bitmask << (nbit + (sizeof(uint_t) * 8 - TBits))) & 0x80000000;
         }
 
         template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::uint64_t>::value, std::nullptr_t>::type = nullptr>
-        inline bool operator [] (std::uint8_t nbit) const{
+        inline bool operator [] (std::uint32_t nbit) const {
             assert(nbit < TBits);
-            return (m_bitmask << (nbit)) & 0x800000000000;
+            return (m_bitmask << (nbit + (sizeof(uint_t) * 8 - TBits))) & 0x8000000000000000;
         }
 
-        inline void set_bit(std::uint8_t nbit) {
-            m_bitmask |= 1UL << ((TBits - 1) - nbit); 
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline bool operator [] (std::uint32_t nbit) const {
+            assert(nbit < TBits);
+            return (m_bitmask[nbit/64] << (nbit + (sizeof(uint_t) * 8 - (TBits - (nbit/64 * 64))))) & 0x8000000000000000;
         }
 
-        inline void unset_bit(std::uint8_t nbit) {
-            m_bitmask &= ~(1UL << ((TBits - 1) - nbit));
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline void set_bit(std::uint32_t nbit) {
+            assert(nbit < TBits);
+            m_bitmask |= 1ULL << ((TBits - 1) - nbit); 
+        }
+        
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline void set_bit(std::uint32_t nbit) {
+            assert(nbit < TBits);
+            std::uint32_t index = nbit/64;
+            m_bitmask[index] |= 1ULL << ((TBits - 1) - nbit - (index * 64)); 
+        }
+
+        template<typename T = uint_t, typename std::enable_if<!std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline void unset_bit(std::uint32_t nbit) {
+            assert(nbit < TBits);
+            m_bitmask &= ~(1ULL << ((TBits - 1) - nbit));
+        }
+
+        template<typename T = uint_t, typename std::enable_if<std::is_same<T, std::array<uint64_t, size()>>::value, std::nullptr_t>::type = nullptr>
+        inline void unset_bit(std::uint32_t nbit) {
+            assert(nbit < TBits);
+            std::uint32_t index = nbit/64;
+            m_bitmask[index] |= ~(1ULL << ((TBits - 1) - nbit - (index * 64)));
         }
 
         Iterator begin() {
-            return Iterator(*this, 0);
+            return Iterator(this, 0);
         }
 
         Iterator end() {
-            return Iterator(*this, TBits);
+            return Iterator(this, TBits);
         }
 
     private:
