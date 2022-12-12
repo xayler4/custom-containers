ead45b4f5018b1caae92b1a0a570c64f6988941b
xayler4 <andrea.pastore04@outlook.com>
2022-12-12 15:35:57 +0100

    minor circular array destructor fixes

--- include/ccnt/circular_array.h
+++ include/ccnt/circular_array.h
@@ -5,7 +5,7 @@
 #include <assert.h>
 
 namespace ccnt  {
-    template<typename TValue, std::uint32_t TSize, typename TAllocator = std::allocator<TValue>>
+    template<typename TValue, std::uint32_t TCapacity, typename TAllocator = std::allocator<TValue>>
     class CircularArray {
     public:
         class Iterator {
@@ -45,47 +45,47 @@ namespace ccnt  {
         };
 
     public:
-        CircularArray() : m_next(m_data){
-            m_data = m_allocator.allocate(TSize);
+        CircularArray() : m_next(m_data), m_count(0) {
+            m_data = m_allocator.allocate(TCapacity);
         };
 
         ~CircularArray() {
             for (std::uint32_t i = 0; i < m_count; i++) {
-                std::destroy_at(m_data[i]);
+                std::destroy_at(m_data + i);
             }
-            m_allocator.deallocate(m_data, TSize);
+            m_allocator.deallocate(m_data, TCapacity);
         }
         
         template<typename... TArgs>
         void emplace(TArgs... args) {
             *(m_next) = std::forward<TValue>(TValue(std::forward<TArgs>(args)...));
-            if (m_next == m_data + TSize - 1) {
+            if (m_next == m_data + TCapacity - 1) {
                 m_next = m_data;
             }
             else {
                 ++m_next;
             }
-            if (!(m_count == TSize)) {
+            if (!(m_count == TCapacity)) {
                 m_count++; 
             }
         }
 
         void push(const TValue& value) {
             *(m_next) = std::move(value);
-            if (m_next == m_data + TSize - 1) {
+            if (m_next == m_data + TCapacity - 1) {
                 m_next = m_data;
             }
             else {
                 ++m_next;
             }
-            if (!(m_count == TSize)) {
+            if (!(m_count == TCapacity)) {
                 m_count++; 
             }
         }
 
         void push(TValue&& value) {
             *(m_next) = std::forward<TValue>(value);
-            if (m_next == m_data + TSize - 1) {
+            if (m_next == m_data + TCapacity - 1) {
                 m_next = m_data;
             }
             else {
@@ -94,12 +94,12 @@ namespace ccnt  {
         }
 
         inline TValue& operator [] (std::uint32_t index) {
-            assert(index >= TSize);
+            assert(index >= TCapacity);
             return m_data[index];
         }
 
-        inline std::uint32_t get_size() const {
-            return TSize;
+        inline std::uint32_t get_capacity() const {
+            return TCapacity;
         }
 
         inline std::uint32_t get_count() const {
@@ -111,7 +111,7 @@ namespace ccnt  {
         }
 
         Iterator end() {
-            return Iterator(m_data + TSize);
+            return Iterator(m_data + TCapacity);
         }
 
     private:
